7a03788c67a305ed6820bb5b25a57f1de8db6c9b
Carlos Amengual <carlosame644+github@gmail.com>
2021-06-26 19:32:41 +0200

    Revert "dom: implement elementsById map as a WeakHashMap in AbstractDocument."
    The map values contain strong references to the map key, so the patch wasn't going to work.
    This reverts commit 27c618ef06639d3bc0b6b545710ea0f0dd582a9e.

--- echosvg-dom/src/main/java/io/sf/carte/echosvg/dom/AbstractDocument.java
+++ echosvg-dom/src/main/java/io/sf/carte/echosvg/dom/AbstractDocument.java
@@ -584,7 +584,7 @@ public abstract class AbstractDocument extends AbstractParentNode
 			return;
 
 		if (elementsById == null) {
-			Map<String, Object> tmp = new WeakHashMap<>();
+			Map<String, Object> tmp = new HashMap<>();
 			tmp.put(id, new IdSoftRef(e, id));
 			elementsById = tmp;
 			return;
