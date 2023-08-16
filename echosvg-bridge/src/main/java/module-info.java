1efe5ee193dccad5ad658219e4fabd541dcd955d
Carlos Amengual <carlosame644+github@gmail.com>
2023-08-16 22:20:55 +0200

    Bump the copyright to year 2023 in several files

--- echosvg-bridge/src/main/java/module-info.java
+++ echosvg-bridge/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Carlos Amengual
+ * Copyright (c) 2020-2023 Carlos Amengual
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
