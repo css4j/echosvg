css4j/echosvg

commit 8164dd76e12a381b7a9714c44286058cc2ceaab8

Use JDK's XPath support #76 (ported from: BATIK-1329: Remove xalan)
Adapted from Batik's commit 490aed3336882aae0eb7c39307f04d6d4b85c4ac by @simonsteiner1984

git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@1903209 13f79535-47bb-0310-9956-ffa450edef68

echosvg-dom/src/main/java/io/sf/carte/echosvg/dom/AbstractDocument.java
@@ -32,10 +32,13 @@ import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.WeakHashMap;
 
-import org.apache.xml.utils.PrefixResolver;
-import org.apache.xpath.XPath;
-import org.apache.xpath.XPathContext;
-import org.apache.xpath.objects.XObject;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.DOMConfiguration;
 import org.w3c.dom.DOMError;
@@ -2007,7 +2010,7 @@ public abstract class AbstractDocument extends AbstractParentNode
 		/**
 		 * The compiled XPath expression.
 		 */
-		protected XPath xpath;
+		protected javax.xml.xpath.XPathExpression xpath;
 
 		/**
 		 * The namespace resolver.
@@ -2020,22 +2023,18 @@ public abstract class AbstractDocument extends AbstractParentNode
 		protected NSPrefixResolver prefixResolver;
 
 		/**
-		 * The XPathContext object.
-		 */
-		protected XPathContext context;
-
-		/**
 		 * Creates a new XPathExpr object.
 		 */
 		public XPathExpr(String expr, XPathNSResolver res) throws DOMException, XPathException {
 			resolver = res;
 			prefixResolver = new NSPrefixResolver();
 			try {
-				xpath = new XPath(expr, null, prefixResolver, XPath.SELECT);
-				context = new XPathContext();
-			} catch (javax.xml.transform.TransformerException te) {
-				throw createXPathException(XPathException.INVALID_EXPRESSION_ERR, "xpath.invalid.expression",
-						new Object[] { expr, te.getMessage() });
+				XPath xPathAPI = XPathFactory.newInstance().newXPath();
+				xPathAPI.setNamespaceContext(prefixResolver);
+				xpath = xPathAPI.compile(expr);
+			} catch (XPathExpressionException te) {
+				throw createXPathException(XPathException.INVALID_EXPRESSION_ERR,
+						"xpath.invalid.expression", new Object[] { expr, te.getMessage() });
 			}
 		}
 
@@ -2063,82 +2062,29 @@ public abstract class AbstractDocument extends AbstractParentNode
 				throw createDOMException(DOMException.NOT_SUPPORTED_ERR, "xpath.invalid.context.node",
 						new Object[] { (int) contextNode.getNodeType(), contextNode.getNodeName() });
 			}
-			context.reset();
-			XObject result = null;
-			try {
-				result = xpath.execute(context, contextNode, prefixResolver);
-			} catch (javax.xml.transform.TransformerException te) {
-				throw createXPathException(XPathException.INVALID_EXPRESSION_ERR, "xpath.error",
-						new Object[] { xpath.getPatternString(), te.getMessage() });
-			}
+
 			try {
 				switch (type) {
 				case XPathResult.ANY_UNORDERED_NODE_TYPE:
 				case XPathResult.FIRST_ORDERED_NODE_TYPE:
-					return convertSingleNode(result, type);
+					return new Result((Node) xpath.evaluate(contextNode, XPathConstants.NODE), type);
 				case XPathResult.BOOLEAN_TYPE:
-					return convertBoolean(result);
+					return new Result((Boolean) xpath.evaluate(contextNode, XPathConstants.BOOLEAN));
 				case XPathResult.NUMBER_TYPE:
-					return convertNumber(result);
+					return new Result((Double) xpath.evaluate(contextNode, XPathConstants.NUMBER));
 				case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
 				case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
 				case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
 				case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
-					return convertNodeIterator(result, type);
+					return new Result((Node) xpath.evaluate(contextNode, XPathConstants.NODE), type);
 				case XPathResult.STRING_TYPE:
-					return convertString(result);
-				case XPathResult.ANY_TYPE:
-					switch (result.getType()) {
-					case XObject.CLASS_BOOLEAN:
-						return convertBoolean(result);
-					case XObject.CLASS_NUMBER:
-						return convertNumber(result);
-					case XObject.CLASS_STRING:
-						return convertString(result);
-					case XObject.CLASS_NODESET:
-						return convertNodeIterator(result, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
-					}
+					return new Result((String) xpath.evaluate(contextNode, XPathConstants.STRING));
 				}
-			} catch (javax.xml.transform.TransformerException te) {
+			} catch (XPathExpressionException | TransformerException te) {
 				throw createXPathException(XPathException.TYPE_ERR, "xpath.cannot.convert.result",
 						new Object[] { (int) type, te.getMessage() });
 			}
 			return null;
-		}
-
-		/**
-		 * Converts an XObject to a single node XPathResult.
-		 */
-		protected Result convertSingleNode(XObject xo, short type) throws javax.xml.transform.TransformerException {
-			return new Result(xo.nodelist().item(0), type);
-		}
-
-		/**
-		 * Converts an XObject to a boolean XPathResult.
-		 */
-		protected Result convertBoolean(XObject xo) throws javax.xml.transform.TransformerException {
-			return new Result(xo.bool());
-		}
-
-		/**
-		 * Converts an XObject to a number XPathResult.
-		 */
-		protected Result convertNumber(XObject xo) throws javax.xml.transform.TransformerException {
-			return new Result(xo.num());
-		}
-
-		/**
-		 * Converts an XObject to a string XPathResult.
-		 */
-		protected Result convertString(XObject xo) {
-			return new Result(xo.str());
-		}
-
-		/**
-		 * Converts an XObject to a node iterator XPathResult.
-		 */
-		protected Result convertNodeIterator(XObject xo, short type) throws javax.xml.transform.TransformerException {
-			return new Result(xo.nodelist(), type);
 		}
 
 		/**
@@ -2327,50 +2273,33 @@ public abstract class AbstractDocument extends AbstractParentNode
 		}
 
 		/**
-		 * Xalan prefix resolver.
+		 * Prefix resolver.
 		 */
-		protected class NSPrefixResolver implements PrefixResolver {
+		protected class NSPrefixResolver implements NamespaceContext {
 
 			/**
-			 * Get the base URI for this resolver. Since this resolver isn't associated with
-			 * a particular node, returns null.
+			 * Resolves the given namespace prefix.
 			 */
 			@Override
-			public String getBaseIdentifier() {
+			public String getNamespaceURI(String prefix) {
+				if (resolver == null) {
+					return null;
+				}
+				return resolver.lookupNamespaceURI(prefix);
+			}
+
+			@Override
+			public String getPrefix(String namespaceURI) {
 				return null;
 			}
 
-			/**
-			 * Resolves the given namespace prefix.
-			 */
 			@Override
-			public String getNamespaceForPrefix(String prefix) {
-				if (resolver == null) {
-					return null;
-				}
-				return resolver.lookupNamespaceURI(prefix);
+			public Iterator<String> getPrefixes(String namespaceURI) {
+				return null;
 			}
 
-			/**
-			 * Resolves the given namespace prefix.
-			 */
-			@Override
-			public String getNamespaceForPrefix(String prefix, Node context) {
-				// ignore the context node
-				if (resolver == null) {
-					return null;
-				}
-				return resolver.lookupNamespaceURI(prefix);
-			}
-
-			/**
-			 * Returns whether this PrefixResolver handles a null prefix.
-			 */
-			@Override
-			public boolean handlesNullPrefixes() {
-				return false;
-			}
 		}
+
 	}
 
 	/**

buildSrc/src/main/groovy/echosvg.java-conventions.gradle
@@ -16,7 +16,6 @@ repositories {
 			releasesOnly()
 		}
 		content {
-			includeGroup "com.github.css4j"
 			includeGroup "io.sf.carte"
 			includeGroup "io.sf.jclf"
 		}

echosvg-bridge/src/main/java/io/sf/carte/echosvg/bridge/svg12/XPathPatternContentSelector.java
@@ -19,10 +19,15 @@
 package io.sf.carte.echosvg.bridge.svg12;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import org.apache.xml.utils.PrefixResolver;
-import org.apache.xpath.XPath;
-import org.apache.xpath.XPathContext;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -48,12 +53,7 @@ public class XPathPatternContentSelector extends AbstractContentSelector {
 	/**
 	 * The XPath expression.
 	 */
-	protected XPath xpath;
-
-	/**
-	 * The XPath context.
-	 */
-	protected XPathContext context;
+	protected XPathExpression xpath;
 
 	/**
 	 * The selected nodes.
@@ -78,13 +78,14 @@ public class XPathPatternContentSelector extends AbstractContentSelector {
 	 * Parses the XPath selector.
 	 */
 	protected void parse() {
-		context = new XPathContext();
 		try {
-			xpath = new XPath(expression, null, prefixResolver, XPath.MATCH);
-		} catch (javax.xml.transform.TransformerException te) {
+			XPath xPathAPI = XPathFactory.newInstance().newXPath();
+			xPathAPI.setNamespaceContext(prefixResolver);
+			xpath = xPathAPI.compile(expression);
+		} catch (XPathExpressionException te) {
 			AbstractDocument doc = (AbstractDocument) contentElement.getOwnerDocument();
-			throw doc.createXPathException(XPathException.INVALID_EXPRESSION_ERR, "xpath.invalid.expression",
-					new Object[] { expression, te.getMessage() });
+			throw doc.createXPathException(XPathException.INVALID_EXPRESSION_ERR,
+					"xpath.invalid.expression", new Object[] { expression, te.getMessage() });
 		}
 	}
 
@@ -165,8 +166,8 @@ public class XPathPatternContentSelector extends AbstractContentSelector {
 		protected void update(Node n) {
 			if (!isSelected(n)) {
 				try {
-					double matchScore = xpath.execute(context, n, prefixResolver).num();
-					if (matchScore != XPath.MATCH_SCORE_NONE) {
+					Double matchScore = (Double) xpath.evaluate(n, XPathConstants.NUMBER);
+					if (matchScore != null) {
 						if (!descendantSelected(n)) {
 							nodes.add(n);
 						}
@@ -177,10 +178,10 @@ public class XPathPatternContentSelector extends AbstractContentSelector {
 							n = n.getNextSibling();
 						}
 					}
-				} catch (javax.xml.transform.TransformerException te) {
+				} catch (XPathExpressionException te) {
 					AbstractDocument doc = (AbstractDocument) contentElement.getOwnerDocument();
-					throw doc.createXPathException(XPathException.INVALID_EXPRESSION_ERR, "xpath.error",
-							new Object[] { expression, te.getMessage() });
+					throw doc.createXPathException(XPathException.INVALID_EXPRESSION_ERR,
+							"xpath.error", new Object[] { expression, te.getMessage() });
 				}
 			}
 		}
@@ -206,42 +207,26 @@ public class XPathPatternContentSelector extends AbstractContentSelector {
 	}
 
 	/**
-	 * Xalan prefix resolver.
+	 * Prefix resolver.
 	 */
-	protected class NSPrefixResolver implements PrefixResolver {
+	protected class NSPrefixResolver implements NamespaceContext {
 
 		/**
-		 * Get the base URI for this resolver. Since this resolver isn't associated with
-		 * a particular node, returns null.
+		 * Resolves the given namespace prefix.
 		 */
 		@Override
-		public String getBaseIdentifier() {
+		public String getNamespaceURI(String prefix) {
+			return contentElement.lookupNamespaceURI(prefix);
+		}
+
+		@Override
+		public String getPrefix(String namespaceURI) {
 			return null;
 		}
 
-		/**
-		 * Resolves the given namespace prefix.
-		 */
 		@Override
-		public String getNamespaceForPrefix(String prefix) {
-			return contentElement.lookupNamespaceURI(prefix);
-		}
-
-		/**
-		 * Resolves the given namespace prefix.
-		 */
-		@Override
-		public String getNamespaceForPrefix(String prefix, Node context) {
-			// ignore the context node
-			return contentElement.lookupNamespaceURI(prefix);
-		}
-
-		/**
-		 * Returns whether this PrefixResolver handles a null prefix.
-		 */
-		@Override
-		public boolean handlesNullPrefixes() {
-			return false;
+		public Iterator<String> getPrefixes(String namespaceURI) {
+			return null;
 		}
 	}
 }

echosvg-dom/build.gradle
@@ -7,7 +7,6 @@ dependencies {
 	api project(':echosvg-css')
 	api project(':echosvg-ext')
 	api project(':echosvg-xml')
-	api "com.github.css4j:echoxsl:${echoXslVersion}"
 	implementation "xml-apis:xml-apis:$xmlApisVersion"
 	api "io.sf.carte:xml-dtd:${xmlDtdVersion}"
 	api "org.apache.xmlgraphics:xmlgraphics-commons:${xmlgraphicsCommonsVersion}"

echosvg-dom/src/main/java/module-info.java
@@ -35,7 +35,6 @@ module io.sf.carte.echosvg.dom {
 	requires transitive java.xml;
 	requires transitive jdk.xml.dom;
 	requires io.sf.carte.xml.dtd;
-	requires com.github.css4j.echoxsl;
 
 	uses org.xml.sax.XMLReader;
 	uses io.sf.carte.echosvg.dom.DomExtension;

gradle.properties
@@ -6,7 +6,6 @@ css4jAwtVersion=4.0
 xmlDtdVersion=4.1.1
 rhinoVersion=1.7.14
 svgomVersion=1.0.1
-echoXslVersion=2.7.3.1
 xmlApisVersion=1.4.01
 xmlgraphicsCommonsVersion=2.8
 htmlParserVersion=1.4.16

commit d1246ffa0f95a8974c885ed64ff1a96ccda271af

dom: fix XPath regression introduced by commit 8164dd76e12 (BATIK-1329: Remove xalan)
Bug reported by Pavel Braginskiy on July 17 to the batik-users mailing list.

echosvg-dom/src/main/java/io/sf/carte/echosvg/dom/AbstractDocument.java
@@ -2122,7 +2122,7 @@ public abstract class AbstractDocument extends AbstractParentNode
 				case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
 				case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
 				case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
-					return new Result((Node) xpath.evaluate(contextNode, XPathConstants.NODE), type);
+					return new Result((NodeList) xpath.evaluate(contextNode, XPathConstants.NODESET), type);
 				case XPathResult.STRING_TYPE:
 					return new Result((String) xpath.evaluate(contextNode, XPathConstants.STRING));
 				}

echosvg-test/src/test/java/io/sf/carte/echosvg/dom/test/XPathTest.java
@@ -1,0 +1,100 @@
+/*
+
+   See the NOTICE file distributed with this work for additional
+   information regarding copyright ownership.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package io.sf.carte.echosvg.dom.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.xpath.XPathEvaluator;
+import org.w3c.dom.xpath.XPathResult;
+
+import io.sf.carte.echosvg.dom.GenericDOMImplementation;
+import io.sf.carte.echosvg.dom.util.SAXDocumentFactory;
+
+/**
+ * XPath tests.
+ */
+public class XPathTest {
+
+	private static final String SVG_DOCUMENT = "<?xml version=\"1.0\" standalone=\"no\"?>\n"
+			+ "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"\n"
+			+ "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n"
+			+ "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"200\" height=\"200\">\n"
+			+ "<rect x=\"5\" y=\"5\" width=\"190\" height=\"190\" fill=\"#daf\" />\n"
+			+ "<text x=\"20\" y=\"30\">foo</text><text x=\"120\" y=\"70\">bar</text>\n</svg>";
+
+	@Test
+	void testXPathIterator() {
+		Document doc = createDocument(SVG_DOCUMENT);
+
+		XPathResult res = (XPathResult) ((XPathEvaluator) doc).evaluate(".//*[local-name()=\"text\"]",
+	            doc.getDocumentElement(), null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+		Node node = res.iterateNext();
+
+		assertNotNull(node, "Node with content 'foo' not found.");
+		assertEquals("foo", node.getTextContent().trim());
+
+		node = res.iterateNext();
+		assertNotNull(node, "Node with content 'bar' not found.");
+		assertEquals("bar", node.getTextContent().trim());
+
+		assertNull(res.iterateNext(), "Unexpected node.");
+	}
+
+	@Test
+	void testXPathSnapshot() {
+		Document doc = createDocument(SVG_DOCUMENT);
+
+		XPathResult res = (XPathResult) ((XPathEvaluator) doc).evaluate(".//*[local-name()=\"text\"]",
+	            doc.getDocumentElement(), null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+		assertEquals(2, res.getSnapshotLength());
+
+		Node node = res.snapshotItem(0);
+
+		assertNotNull(node, "Node with content 'foo' not found.");
+		assertEquals("foo", node.getTextContent().trim());
+
+		node = res.snapshotItem(1);
+		assertNotNull(node, "Node with content 'bar' not found.");
+		assertEquals("bar", node.getTextContent().trim());
+
+		assertNull(res.snapshotItem(2), "Unexpected node.");
+	}
+
+	private static Document createDocument(String xml) {
+		StringReader re = new StringReader(xml);
+		SAXDocumentFactory f = new SAXDocumentFactory(GenericDOMImplementation.getDOMImplementation());
+		f.setValidating(false);
+		try {
+			return f.createDocument("file:///memory", re);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}