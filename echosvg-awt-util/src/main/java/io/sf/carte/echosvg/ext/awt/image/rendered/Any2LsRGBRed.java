2668bab9695170efd6a77220f5dc35ad68983c75
Carlos Amengual <carlosame644+github@gmail.com>
2024-08-27 19:26:36 +0200

    awt: convert images to linear sRGB according to IEC 61966-2-1

--- echosvg-awt-util/src/main/java/io/sf/carte/echosvg/ext/awt/image/rendered/Any2LsRGBRed.java
+++ echosvg-awt-util/src/main/java/io/sf/carte/echosvg/ext/awt/image/rendered/Any2LsRGBRed.java
@@ -69,7 +69,7 @@ public class Any2LsRGBRed extends AbstractRed {
 	private static final double LFACT = 1.0 / 12.92;
 
 	public static final double sRGBToLsRGB(double value) {
-		if (value <= 0.003928)
+		if (value <= 0.04045d)
 			return value * LFACT;
 		return Math.pow((value + 0.055) / 1.055, GAMMA);
 	}
