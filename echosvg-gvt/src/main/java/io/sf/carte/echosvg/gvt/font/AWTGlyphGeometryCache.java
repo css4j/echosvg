e6b8d90234adba439916dfe8546f99dfc373fe18
Carlos Amengual <carlosame644+github@gmail.com>
2025-11-14 16:37:12 +0100

    gvt: fix a get() race condition in b8a747b08effe2c80093633eb04ce89986ffc27b

--- echosvg-gvt/src/main/java/io/sf/carte/echosvg/gvt/font/AWTGlyphGeometryCache.java
+++ echosvg-gvt/src/main/java/io/sf/carte/echosvg/gvt/font/AWTGlyphGeometryCache.java
@@ -173,23 +173,31 @@ public class AWTGlyphGeometryCache {
 	}
 
 	/**
-	 * Rehash the table
+	 * Rehash the table.
+	 * <p>
+	 * This method should be called under a lock.
+	 * </p>
 	 */
 	protected void rehash() {
 		Entry[] oldTable = table;
+		int olen = oldTable.length;
 
-		table = new Entry[oldTable.length * 2 + 1];
+		int rehlen = olen * 2 + 1;
+		Entry[] rehTable = new Entry[rehlen];
 
-		for (int i = oldTable.length - 1; i >= 0; i--) {
-			for (Entry old = oldTable[i]; old != null;) {
+		for (int i = olen - 1; i >= 0; i--) {
+			Entry old = oldTable[i];
+			while (old != null) {
 				Entry e = old;
 				old = old.next;
 
-				int index = e.hash % table.length;
-				e.next = table[index];
-				table[index] = e;
+				int index = e.hash % rehlen;
+				e.next = rehTable[index];
+				rehTable[index] = e;
 			}
 		}
+
+		table = rehTable;
 	}
 
 	/**
